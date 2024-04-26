e50f0e6af3bd52abc2fb040e0d6c46b9d41217c8
Susmita Ghosh <susmitaranighosh03@gmail.com>
2024-04-26 01:59:52 -0400

    State Management - Preparation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,18 @@
 import classes from './PostList.module.css';
 import Post from './Post';
+import NewPost from './NewPost';
 
 function PostList() {
     return (
+        <>
+        <NewPost />
         <ul className={classes.Postlist}>
             
                 <Post author= "Michael" body="React is great"/>
                 <Post author= "Jhon" body= "React is awesome"/>
             
         </ul>
+        </>
     );
 }
 
