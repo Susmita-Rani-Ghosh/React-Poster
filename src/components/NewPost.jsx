458153087b4f52f58ad6a57b881de66d2484a601
Susmita Ghosh <susmitaranighosh03@gmail.com>
2024-05-10 01:46:18 -0400

    SetUp Event Listeners- Onchnage

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,11 +1,22 @@
 import classes from './NewPost.module.css';
-
+//in this newPost we want to listen to keyStores in the Text area
+//To do this we will set up an event listener
 function NewPost() {
+    // in Vallina javascript- Event Listener looks like this
+    // document.querySelector('textarea').addEventListener('change', function () {});
+
+    // in React we can do this by adding a Property to the area where we want to listen.
+        //e.g.: onKeyDown or onChnage
+    // Function to be used for onChnage event lister
+    function changeBodyHandler(event) { //this function will be executed evenever a key is pressed on the event listener.
+        console.log(event.target.value);
+    }
+
     return(
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea  id="body" required rows={3} />
+                <textarea  id="body" required rows={3} onChnage={changeBodyHandler}/>
             </p>
             <p>
                 <label htmlFor="name">Your name</label>
